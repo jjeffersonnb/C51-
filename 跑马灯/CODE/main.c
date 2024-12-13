73e522fc64e6080aadefbfc233cf929f1e138289
JEFFERSON POON <127844446+jjeffersonnb@users.noreply.github.com>
2024-12-13 14:19:11 +0800

    Add files via upload

---
+++ 跑马灯/CODE/main.c
@@ -0,0 +1,19 @@
+#include<reg51.h>
+#include<intrins.h> 
+#define uchar unsigned char
+#define uint unsigned int
+	void DelayMS(uint x)
+	{
+			uchar i;
+	while(x--)
+		for(i=120;i>0;i--);
+	}
+	void main()
+	{
+		P0=0xFE;
+	  while(1)
+		{
+			P0=_crol_(P0,1);
+		  DelayMS(150);
+		}
+	}
